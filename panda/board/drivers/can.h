f0c8f1f8d23fa0fba4af67e9f499107f79429e39
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-10-17 23:39:16 +0300

    Update can.h

--- panda/board/drivers/can.h
+++ panda/board/drivers/can.h
@@ -124,6 +124,7 @@ uint8_t bus_lookup[] = {0,1,2};
 uint8_t can_num_lookup[] = {0,1,2,-1};
 int8_t can_forwarding[] = {-1,-1,-1,-1};
 uint32_t can_speed[] = {5000, 5000, 5000, 333};
+#define CAN_MAX 1U
 
 #define CANIF_FROM_CAN_NUM(num) (cans[num])
 #define CAN_NUM_FROM_CANIF(CAN) ((CAN)==CAN1 ? 0 : ((CAN) == CAN2 ? 1 : 2))
